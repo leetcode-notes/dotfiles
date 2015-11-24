7debba9c529bbf34d11c3ba0f208bb2b3fd2f798
Raghav Kumar Gautam <raghavgautam@users.noreply.github.com>
2015-11-23 20:41:40 -0800

    adding installation of basic tools

--- setup.sh
+++ setup.sh
@@ -8,3 +8,23 @@ mkdir ~/.emacs.d/
 rm -rf ~/.emacs.d/lisp
 mv /tmp/`whoami`/init-master/* /tmp/`whoami`/init-master/.[^.]* ~/.emacs.d/
 ~/.emacs.d/setup.sh
+
+#install packages
+ME=`whoami`
+PKGS="emacs screen tree git tmux"
+if [ "$ME" == "root" ]; then
+    if hash yum 2>/dev/null; then
+        yum -y install $PKGS
+    elif hash apt-get 2>/dev/null; then
+        apt-get -y install $PKGS
+    elif hash zypper 2>/dev/null; then
+        if [ -f "/etc/SuSE-release" ]; then
+            zypper addrepo -n -f http://download.opensuse.org/distribution/11.4/repo/oss/ oss
+        fi
+        zypper -n install $PKGS
+    else
+        echo "unknown package manager"
+    fi
+else
+    echo "not installing anything"
+fi
