efd017dc2609427dc91f90658f9ef7c5bbbff652
Raghav Kumar Gautam <raghavgautam@users.noreply.github.com>
2015-11-23 20:25:07 -0800

    adding setup.sh file

---
+++ setup.sh
@@ -0,0 +1,7 @@
+curl -L https://github.com/raghavgautam/init/archive/master.zip -o /tmp/`whoami`.zip
+unzip -o -d /tmp/`whoami`/ /tmp/`whoami`.zip
+chmod -R 777 /tmp/`whoami`/
+mkdir ~/.emacs.d/
+rm -rf ~/.emacs.d/lisp
+mv /tmp/`whoami`/init-master/* /tmp/`whoami`/init-master/.[^.]* ~/.emacs.d/
+~/.emacs.d/setup.sh
