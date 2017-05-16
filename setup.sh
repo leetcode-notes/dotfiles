4c08a0db82199fe68b43b41af61c2a866b4253be
Raghav Kumar Gautam <raghav@apache.org>
2017-05-16 09:44:30 -0700

    for robustness - install one package at a time

--- setup.sh
+++ setup.sh
@@ -9,17 +9,17 @@ mv /tmp/`whoami`/init-master/* /tmp/`whoami`/init-master/.[^.]* ~/.emacs.d/
 ~/.emacs.d/setup.sh
 
 #install packages
-PKGS="emacs screen tree git tmux"
+PKGS=(emacs screen tree git tmux)
 if ( sudo -v ); then
     if hash yum 2>/dev/null; then
-        sudo yum -y install $PKGS
+        for p in "${PKGS[@]}"; do sudo yum -y install "$p"; done
     elif hash apt-get 2>/dev/null; then
-        sudo apt-get -y install $PKGS
+        for p in "${PKGS[@]}"; do sudo apt-get -y install "$p"; done
     elif hash zypper 2>/dev/null; then
         if [ -f "/etc/SuSE-release" ]; then
-            sudo zypper addrepo -n -f http://download.opensuse.org/distribution/11.4/repo/oss/ oss
+            sudo zypper --non-interactive addrepo http://download.opensuse.org/distribution/11.4/repo/oss/ oss
         fi
-        sudo zypper --no-gpg-checks -n install $PKGS
+        for p in "${PKGS[@]}"; do sudo zypper --non-interactive --no-gpg-checks -n install "$p"; done
     else
         echo "unknown package manager"
     fi
